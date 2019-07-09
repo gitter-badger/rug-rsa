40f1f046be82aa05cabed30a1ff32957f3ab10e4
Yves Lemée <lesavonfou@gmail.com>
2019-07-09 19:36:07 +0200

    first draft

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,70 @@
 extern crate rug;
-mod rsa;
+pub mod rsa;
 
-pub use self::rsa::*;
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     use rug::Integer;
 
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn create_rsa_object() {
+        let p = Integer::from(61);
+        let q = Integer::from(53);
+        let e = Integer::from(17);
+        let n = Integer::from(3233);
+
+        let key = rsa::PublicKey::new(p, q, e.clone()).unwrap();
+        
+        assert_eq!(&n, key.n());
+        assert_eq!(&e, key.e());
     }
 
     #[test]
-    fn load_large_number_in_integer_type() {
-// 10156250692207502789356668972415053328118344801273899866955858837342856420136490741681197453320753350433376819335908540396974632472615931364041200905644673
-    
-        let big_number = Integer::from_str_radix("10156250692207502789356668972415053328118344801273899866955858837342856420136490741681197453320753350433376819335908540396974632472615931364041200905644", 10).unwrap();
-        assert_eq!(502, big_number.significant_bits())
+    fn create_private_key() {
+        let p = Integer::from(61);
+        let q = Integer::from(53);
+        let e = Integer::from(17);
+        let d = Integer::from(413);
+        let keypair = rsa::KeyPair::from_primes(p, q, e).unwrap();
+
+        assert_eq!(&d, keypair.private_key().d());
+    }
+
+    #[test]
+    fn test_generate_a_keypair() {
+        let m = Integer::from(65);
+        let keypair = rsa::KeyPair::gen(4096, None).unwrap();
+
+        let ciphertext = keypair.public_key().encrypt(&m).unwrap();
+        let plaintext = keypair.private_key().decrypt(&ciphertext).unwrap();
+
+        assert_eq!(65, plaintext);
+    }
+
+    #[test]
+    fn encrypt_message_with_several_numbers() {
+        let p = Integer::from(61);
+        let q = Integer::from(53);
+        let e = Integer::from(17);
+        let m = Integer::from(65);
+        let c = Integer::from(2790);
+        let keypair = rsa::KeyPair::from_primes(p, q, e).unwrap();
+        let ciphertext = keypair.public_key().encrypt(&m).unwrap();
+
+        assert_eq!(c, ciphertext);
+    }
+
+    #[test]
+    fn decrypt_message_with_small_numbers() {
+        let p = Integer::from(61);
+        let q = Integer::from(53);
+        let e = Integer::from(17);
+        let m = Integer::from(65);
+        let c = Integer::from(2790);
+        let keypair = rsa::KeyPair::from_primes(p, q, e).unwrap();
+        let plaintext = keypair.private_key().decrypt(&c).unwrap();
+
+        assert_eq!(m, plaintext);
     }
 }
--- src/rsa.rs
+++ src/rsa.rs
@@ -1,12 +1,26 @@
 extern crate rug;
+extern crate rand;
+extern crate rayon;
 
-use rug::Integer;
+use rug::rand::RandState;
+use rug::{Assign, Integer};
+use rug::integer::Order;
+use rand::prelude::*;
+use rayon::prelude::*;
 
+#[derive(Debug)]
+pub enum RsaError {
+    InitError,
+    PrimesInvalid,
+}
+
+#[derive(Debug)]
 pub struct PublicKey {
     n: Integer,
     e: Integer,
 }
 
+#[derive(Debug)]
 pub struct PrivateKey {
     n: Integer,
     e: Integer,
@@ -18,21 +32,108 @@ pub struct KeyPair {
     public: PublicKey
 }
 
-pub trait PublicKeyTrait {
-    fn n(&self) -> &Integer;
-    fn e(&self) -> &Integer;
+fn generate_a_big_number(bit_length: u32) -> Integer {
+    let mut rand = RandState::new();
+    let mut i = Integer::from(Integer::random_bits(0, &mut rand));
+    let random_vector = (1..bit_length/8)
+        .into_par_iter()
+        .map(|_| {
+            rand::thread_rng().gen::<u8>()
+        })
+        .collect::<Vec<u8>>();
 
-    fn key_length(&self) -> usize {
-        0
-    }
+    i.assign_digits(&random_vector, Order::Msf);
+    i 
 }
 
-impl PublicKeyTrait for PublicKey {
-    fn n(&self) -> &Integer {
+fn generate_p_and_q(key_length: u32, p: &mut Integer, q: &mut Integer) -> Result<(), RsaError> {
+        // generate a prime number with length inferior to target key length
+        p.assign( generate_a_big_number(3*(key_length/4)));
+        p.next_prime_mut();
+        
+
+        q.assign( generate_a_big_number(3*(key_length/4)));
+        q.next_prime_mut();
+
+        let n = p.clone() * q.clone();
+        if n.significant_bits() <= key_length {
+            Err(RsaError::PrimesInvalid)
+        } else {
+            Ok(())
+        }
+
+}
+
+impl PublicKey {
+    pub fn new(p: Integer, q: Integer, e: Integer) -> Result<Self, RsaError> {
+        let n = p * q;
+        let public_key = PublicKey{n, e};
+        // check if key is valid
+
+        Ok(public_key)
+    }
+
+    pub fn n(&self) -> &Integer {
         &self.n
     }
 
-    fn e(&self) -> &Integer {
-        &self.n
+    pub fn e(&self) -> &Integer {
+        &self.e
+    }
+
+    pub fn encrypt(&self, m: &Integer) -> Result<Integer, RsaError> {
+        let c = m.clone().pow_mod(&self.e, &self.n).unwrap();
+        Ok(c)
     }
 }
+
+impl PrivateKey {
+    pub fn new(p: Integer, q: Integer, e: Integer) -> Result<Self, RsaError> {
+        let n = q.clone() * p.clone();
+        let lambda_n: Integer = p - 1;
+        let lambda_n = lambda_n.lcm(&(q-1));
+        let d = e.clone().invert(&lambda_n).unwrap();
+        let private_key = PrivateKey{n, e, d};
+        Ok(private_key)
+    }
+
+    pub fn d(&self) -> &Integer {
+        &self.d
+    }
+
+    pub fn decrypt(&self, c: &Integer) -> Result<Integer, RsaError> {
+        let m = c.clone().pow_mod(&self.d, &self.n).unwrap();
+        Ok(m)
+    }
+}
+
+impl KeyPair {
+
+    pub fn gen(key_length: u32, e: Option<u32>) -> Result<Self, RsaError> {
+        let e = match e {
+            Some(e) => Integer::from(e),
+            None => Integer::from(65537),
+        };
+        let mut p = Integer::new();
+        let mut q = Integer::new();
+        while generate_p_and_q(key_length, &mut p, &mut q).is_err() {}
+
+        KeyPair::from_primes(p, q, e)
+    }
+
+    pub fn from_primes(p: Integer, q: Integer, e: Integer) -> Result<Self, RsaError> {
+        let private = PrivateKey::new(p.clone(), q.clone(), e.clone())?;
+        let public = PublicKey::new(p, q, e)?;
+        let keypair = KeyPair{private, public};
+        Ok(keypair)
+    }
+
+    pub fn public_key(&self) -> &PublicKey {
+        &self.public
+    }
+
+    pub fn private_key(&self) -> &PrivateKey {
+        &self.private
+    }
+}
+
